c46b2d17dade7837a9abba23a152b4b7f843f3c1
Mohammed27800 <asmte2780@gmail.com>
2025-04-25 18:04:40 +0300

    renaming title

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ model = joblib.load('svm_model.joblib')
 scaler = joblib.load('scaler.joblib')
 
 # إنشاء واجهة Streamlit
-st.title("تنبؤ الأنيميا باستخدام نموذج تعلم الآلة")
+st.title("'SVM' التنبؤ بالانيميا باستخدام خوارزمية ال")
 
 # مدخلات المستخدم مع قيم افتراضية
 gender = st.selectbox("الجنس:", [0, 1], format_func=lambda x: "أنثى" if x == 0 else "ذكر", index=1)  # 0: أنثى, 1: ذكر
-hemoglobin = st.number_input("الهيموجلوبين:", min_value=0.0, value=13.0)  # القيمة الافتراضية للهيموجلوبين
+hemoglobin = st.number_input("الهيموجلوبين:", min_value=0.0, value=20.0)  # القيمة الافتراضية للهيموجلوبين
 mch = st.number_input("MCH:", min_value=0.0, value=25.0)  # القيمة الافتراضية لـ MCH
-mchc = st.number_input("MCHC:", min_value=0.0, value=32.0)  # القيمة الافتراضية لـ MCHC
-mcv = st.number_input("MCV:", min_value=0.0, value=85.0)  # القيمة الافتراضية لـ MCV
+mchc = st.number_input("MCHC:", min_value=0.0, value=80.0)  # القيمة الافتراضية لـ MCHC
+mcv = st.number_input("MCV:", min_value=0.0, value=30.0)  # القيمة الافتراضية لـ MCV
 
 # عند الضغط على الزر، نقوم بالتنبؤ
 if st.button("تنبؤ"):
